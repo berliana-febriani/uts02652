3ac992b50f12f6ee0c07940bd0fdb013efd952fc
berliana-febriani <62787722+berliana-febriani@users.noreply.github.com>
2020-04-17 11:56:59 +0700

    Add files via upload

---
+++ modules/beranda/controllers/Beranda.php
@@ -0,0 +1,36 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+ 
+class Beranda extends CI_Controller { 
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('cart');
+		$this->load->model('keranjang_model');
+	}
+ 
+ 	public function index(){ 
+ 		$data['produk'] = $this->keranjang_model->get_produk_all();
+ 		$data['kategori'] = $this->keranjang_model->get_kategori_all();
+ 		$this->load->view('themes/navbar',$data);
+ 		$this->load->view('themes/kategori', $data);   
+ 		$this->load->view('shopping/list_produk', $data);
+ 		$this->load->view('themes/footer', $data);
+	} 
+
+ 	public function tentang(){
+ 		$data['kategori'] = $this->keranjang_model->get_kategori_all();
+ 		$this->load->view('themes/navbar',$data);
+ 		$this->load->view('themes/kategori', $data);
+ 		$this->load->view('tentangv', $data);   
+ 		$this->load->view('themes/footer', $data);
+ 	}
+
+ 	public function cara_bayar(){
+ 		$data['kategori'] = $this->keranjang_model->get_kategori_all();
+ 		$this->load->view('themes/navbar',$data);
+ 		$this->load->view('themes/kategori', $data);
+ 		$this->load->view('cara_bayarv', $data);   
+ 		$this->load->view('themes/footer', $data);
+ 	}
+}
